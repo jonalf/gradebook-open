91dee49f4159a436aee024377f8f82cf4527063e
JonAlf <jonalf@gmail.com>
2018-01-29 16:38:37 -0500

    dataConvert modified to account for new input file formats

--- data/dataConvert.py
+++ data/dataConvert.py
@@ -3,14 +3,14 @@ import sys
 
 #USE THIS VERSION FOR NON DEADALUS IMPORTS
 
-CLASS = 0
-SECTION = 1
-FIRST = 6
-LAST = 5
-OSIS = 7
-TEACHER = 8
-HR = 10
-PERIOD = 12
+CLASS = 5
+SECTION = 6
+FIRST = 2
+LAST = 1
+OSIS = 0
+TEACHER = 7
+HR = 3
+PERIOD = 9
 
 def dataConvert( filename ):
     f = open(filename)
@@ -20,7 +20,7 @@ def dataConvert( filename ):
     f.close()
     for i in range( len(lines) ):
         l = lines[i].split(',')
-        lines[i] = [ l[CLASS], l[SECTION], l[PERIOD], l[TEACHER], l[LAST], l[FIRST], l[OSIS], l[HR], l[OSIS], 'NA']
+        lines[i] = [ l[CLASS], l[SECTION], l[PERIOD], l[TEACHER], l[LAST].capitalize(), l[FIRST].capitalize(), l[OSIS], l[HR], l[OSIS], 'NA']
         #lines[i] = [ l[5], l[6], l[9], l[7], l[1].title(), l[2].title(), l[0], l[3], l[0], 'NA']
     return lines
 
